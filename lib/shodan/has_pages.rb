dff5a8994393652e2113464ffc9181cfe9e29869
postmodern <postmodern.mod3@gmail.com>
2009-12-03 08:21:12 -0800

    Added YARD tags to HasPages.

--- lib/shodan/has_pages.rb
+++ lib/shodan/has_pages.rb
@@ -23,37 +23,68 @@ module Shodan
     include Enumerable
 
     #
-    # Returns the first page.
+    # The first page.
+    #
+    # @return [Page]
+    #   The first page.
     #
     def first_page
       page_cache[1]
     end
 
     #
-    # Returns the page at the specified _index_.
+    # The page at a given index.
+    #
+    # @param [Integer] index
+    #   The index to request a page for.
+    #
+    # @return [Page]
+    #   The page at the specified index.
     #
     def [](index)
       page_cache[index]
     end
 
     #
-    # Returns the pages with the specified _indices_.
+    # The pages at the given indices.
+    #
+    # @param [Range, Array<Integer>]
+    #   The indices to request pages for.
+    #
+    # @return [Array<Page>]
+    #   The pages at the specified indices.
     #
     def pages(indices)
       indices.map { |index| page_cache[index] }
     end
 
     #
-    # Iterates over the pages with the specified _indices_, passing each
-    # to the specified _block_.
+    # Iterates over the pages at given indices.
+    #
+    # @param [Range, Array<Integer>]
+    #   The indices to request pages for.
+    #
+    # @yield [page]
+    #   The given block will be passed each page at one of the specified
+    #   indices.
+    #
+    # @yieldparam [Page] page
+    #   The page at one of the specified indices.
     #
     def each_page(indices,&block)
       indices.map { |index| block.call(page_cache[index]) }
     end
 
     #
-    # Iterates over all the pages of the query, passing each to the
-    # specified _block_.
+    # Iterates over every page, until an empty page is encountered.
+    #
+    # @yield [page]
+    #   The given block will receive every non-empty page.
+    #
+    # @yieldparam [Page] page
+    #   A non-empty page.
+    #
+    # @return [self]
     #
     def each(&block)
       index = 1
@@ -67,38 +98,72 @@ module Shodan
     end
 
     #
-    # Iterates over all hosts on all pages of the query, passing each host
-    # to the given _block_.
+    # Iterates over every host.
+    #
+    # @yield [host]
+    #   If a block is given, it will be passed every host from every page.
+    #
+    # @yieldparam [Host] host
+    #   A host.
+    #
+    # @return [self]
     #
     def each_host(&block)
       each { |page| page.each(&block) }
     end
 
     #
-    # Iterates over the elements on the page with the specified _index_,
-    # passing each element to the specified _block_.
+    # Iterates over the hosts in a page at a given index.
+    #
+    # @param [Integer] index
+    #   The index of the page to iterate over.
+    #
+    # @yield [host]
+    #   If a block is given, it will be passed every host on the page at
+    #   the specified index.
+    #
+    # @yieldparam [Host] host
+    #   A host on the page at the specified index.
     #
     def each_on_page(index,&block)
       page_cache[index].each(&block)
     end
 
     #
-    # Iterates over each element on the pages with the specified _indices_,
-    # passing each element to the specified _block_.
+    # Iterates over each host on the pages at the given indices.
+    #
+    # @param [Range, Array<Integer>] indices
+    #   The indices of the pages.
+    #
+    # @yield [host]
+    #   If a block is given, it will be passed ever host on the pages
+    #   at the specified indices.
+    #
+    # @yieldparam [Host] host
+    #   A host on one of the pages at the specified indices.
     #
     def each_on_pages(indices,&block)
       each_page(indices) { |page| page.each(&block) }
     end
 
     #
-    # Returns the first Result on the first_page.
+    # The first host on the first page.
+    #
+    # @return [Host]
+    #   The first host.
     #
     def first_host
       first_page.first
     end
 
     #
-    # Returns the Result at the specified _index_.
+    # Returns the host at a given index.
+    #
+    # @param [Integer] index
+    #   The index to request the host at.
+    #
+    # @return [Host]
+    #   The host at the given index.
     #
     def host_at(index)
       page(page_index_of(index))[result_index_of(index)]
@@ -107,28 +172,52 @@ module Shodan
     protected
 
     #
-    # Returns the page index for the specified result _rank_.
+    # Calculates the page index for a given result rank.
+    #
+    # @param [Integer]
+    #   The rank of a result within the total results.
+    #
+    # @return [Integer]
+    #   The index of the page that will contain the result, with the
+    #   specified rank.
     #
     def page_index_of(rank)
       (((rank.to_i - 1) / results_per_page.to_i) + 1)
     end
 
     #
-    # Returns the rank offset for the specified _page_index_.
+    # Calculates the result rank of the first result on the page at the
+    # given index.
+    #
+    # @param [Integer] page_index
+    #   The index of a page.
+    #
+    # @return [Integer]
+    #   The rank of the first result on the page, with the specified index.
     #
     def result_offset_of(page_index)
       ((page_index.to_i - 1) * results_per_page.to_i)
     end
 
     #
-    # Returns the in-page index of the specified result _rank_.
+    # Calculates the in-page index of a given result rank.
+    #
+    # @param [Integer] rank
+    #   The result rank.
+    #
+    # @return [Integer]
+    #   The index of the result, within the page that will contain the
+    #   result.
     #
     def result_index_of(rank)
       ((rank.to_i - 1) % results_per_page.to_i)
     end
 
     #
-    # The cache of previously requested pages.
+    # The page cache.
+    #
+    # @return [Hash{Integer => Page}]
+    #   A persistant Hash of pages.
     #
     def page_cache
       @page_cache ||= Hash.new { |hash,key| hash[key] = page(key.to_i) }
